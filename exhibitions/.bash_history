65866ff1b1f16ddaf274ca875127a9c2f1ff0073
Kevin Reiss. <kevin.reiss@gmail.com>
2024-07-29 09:05:59 -0500

    Add archived exhibits from libserv39.

---
+++ exhibitions/.bash_history
@@ -0,0 +1,10 @@
+cd MagicLantern
+vi ml6.html
+ls
+vi ML6.html
+logout
+ls
+cd MagicLantern
+vi ML5.html
+vi ML11.html
+logout
---
+++ exhibitions/CreepyCrawlies/JsCSS/CreepyCrawlies.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function wopen(url, name, w, h)
+{
+// Fudge factors for window decoration space.
+w += 32;
+h += 96;
+ var win = window.open(url,
+  name, 
+  'width=' + w + ', height=' + h + ', ' +
+  'location=no, menubar=no, ' +
+  'status=no, toolbar=no, scrollbars=no, resizable=no');
+ win.resizeTo(w, h);
+ win.focus();
+}
